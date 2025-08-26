34885c25bcbf3e88d209772e782cf75d853c1217
uma <umamaheswari.g@everest.engineering>
2025-08-26 09:57:26 +0530

    Add multiple endpoints to the express server

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,10 @@ app.get('/', (_req, res) => {
   res.send('Hello Mahi! Welcome to the application')
 })
 
+app.get('/uma', (_req, res) => {
+  res.send('Hello uma! Welcome to the application')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
