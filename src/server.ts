2c509bfb6eb3c378124a97892fe5159daac44111
uma <umamaheswari.g@everest.engineering>
2025-08-26 09:56:49 +0530

    Add the initial express server with root endpoint

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express'
+const app = express()
+const port = 3000
+
+app.get('/', (_req, res) => {
+  res.send('Hello Mahi! Welcome to the application')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
